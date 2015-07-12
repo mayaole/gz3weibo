4fd176c239dd0697cad486a07c7ec606c756a36a
apple <apple@appledeiMac.local>
2015-07-12 14:50:36 +0800

    完成模块1

--- Weibo/Weibo/ViewController.m
+++ Weibo/Weibo/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 完成模块1
 }
 
 - (void)didReceiveMemoryWarning {
